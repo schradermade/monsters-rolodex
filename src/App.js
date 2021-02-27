700d2146c7c2036cc635a9061f8ab829b6b949a5
Nathan Schrader <nathanschrader@icloud.com>
2021-02-26 16:35:56 -0800

    refactor onChange method

--- src/App.js
+++ src/App.js
@@ -19,17 +19,19 @@ class App extends React.Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className='App'>
         <input 
           type='search'
           placeholder='search monsters' 
-          onChange={e => {
-            this.setState({ searchField: e.target.value }, () => 
-            console.log(this.state));
-          }}
+          onChange={ e =>
+            this.setState({ searchField: e.target.value })}
           />
-        <CardList monsters={this.state.monsters}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
